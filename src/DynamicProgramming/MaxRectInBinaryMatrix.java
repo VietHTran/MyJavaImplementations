1e2cb05c29a90b9186aabb40a129f74db849567e
VietHTran <thanos.trn11@gmail.com>
2016-11-03 23:52:04 -0500

    Add Max Rectangle in Binary Matrix Solution

---
+++ src/DynamicProgramming/MaxRectInBinaryMatrix.java
@@ -0,0 +1,93 @@
+package DynamicProgramming;
+
+import java.util.ArrayList;
+
+//Problems: Given a 2D binary matrix filled with 0’s and 1’s
+//Find the largest rectangle containing all ones and return its area.
+//Example:
+//1 1 1
+//0 1 1
+//1 0 0
+//Answer: 4 (point (0,1) and point (2,2) form an 4x4 all-one rectangle)
+public class MaxRectInBinaryMatrix {
+	public int maximalRectangle(ArrayList<ArrayList<Integer>> a) {
+	    if (a==null || a.size()==0) {
+	        return 0;
+	    }
+	    int max=0;
+	    int cur=0;
+	    ArrayList<Integer> holder=a.get(0);
+	    //Calculate the maximum area in the first area
+	    for (int i=0;i<holder.size();i++) {
+	        if (holder.get(i)==1) {
+	            cur++;
+	        } else {
+	            if (cur>max) {
+	                max=cur;
+	            }
+	            cur=0;
+	        }
+	    }
+	    if (cur>max) {
+	        max=cur;
+	    }
+	    
+	    for (int i=1;i<a.size();i++) {
+	        holder=a.get(i);
+	        //streak[i] store the continuous occurrence of 
+	        //columns that has number of 1s appear continuously 
+	        //from the rows above to the current row is >=i
+	        int[] streak= new int[i+2];
+	        for (int k=0;k<holder.size();k++) {
+	            if (holder.get(k)==1) {
+	            	//Number of consecutive columns with 1s + 1
+	                holder.set(k,a.get(i-1).get(k)+1);
+	                //Length of column of 1s
+	                cur=holder.get(k);
+	                //Update the column 1s streak
+	                for (int j=cur;j>=1;j--) {
+	                    streak[j]++;
+	                }
+	                //If the previous column of 1s streak breaks when previous row's length is higher the current ones
+	                if (k>0 && holder.get(k-1)>cur){
+	                    int prev=holder.get(k-1);
+	                    for (int u=prev;u>cur;u--) {
+	                    	//Calculate and update area if necessary
+	                    	//u is length of column of 1s (height)
+	                    	//streaks[u] is columns of 1s in current row (width) 
+	                        int area=u*streak[u];
+	                        if (area>max) {
+	                            max=area;
+	                        }
+	                        //Reset streak
+	                        streak[u]=0;
+	                    }
+	                }
+	            } else {
+	            	//If encounter 0 --> Reset all column streaks
+	                for (int u=1;u<streak.length;u++) {
+	                    if (streak[u]==0) {
+	                        continue;
+	                    }
+	                    int area=u*streak[u];
+	                    if (area>max) {
+	                        max=area;
+	                    }
+	                    streak[u]=0;
+	                }
+	            }
+	        }
+	        //Check all the remaining streaks
+	        for (int k=1;k<streak.length;k++) {
+	            if (streak[k]==0) {
+	                continue;
+	            }
+	            int area=k*streak[k];
+	            if (area>max) {
+	                max=area;
+	            }
+	        }
+	    }
+	    return max;
+	}
+}
--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,16 +1,27 @@
 import java.*;
+import java.io.IOException;
 import java.util.*;
-import SegmentTree.*;
+import DynamicProgramming.*;
 
 public class MainActivity {
-	public static void main(String[] args) {
-		Integer[] h={5,3,2,6,1,4};
-		Integer[] iF={0,1,2,0,3,2};
-		ArrayList<Integer> hL= new ArrayList<Integer> (Arrays.asList(h));
-		ArrayList<Integer> iFL= new ArrayList<Integer> (Arrays.asList(iF));
-		ArrayList<Integer> res=SegmentTreeSolution.OrderByPeopleHeights(hL,iFL);
-		for (int i=0;i<res.size();i++) {
-			System.out.println(res.get(i));
+	public static void main(String[] args) throws IOException {
+		ArrayList<ArrayList<Integer>> test= new ArrayList<ArrayList<Integer>>();
+		Scanner s= new Scanner(System.in);
+		System.out.println("Number of rows: ");
+		int n=s.nextInt();
+		System.out.println("Number of columns: ");
+		int m=s.nextInt();
+		for (int i=0;i<n;i++) {
+			ArrayList<Integer> holder= new ArrayList<Integer>();
+			for (int k=0;k<m;k++) {
+				int u=s.nextInt();
+				if (u!=1 && u!=0) {
+					throw new IOException("Input can only 1 and 0 only");
+				}
+				holder.add(u);
+			}
+			test.add(holder);
 		}
+		System.out.println("Max Rectangle: "+(new MaxRectInBinaryMatrix()).maximalRectangle(test));
 	}
 }
